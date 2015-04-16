8c5ecb59f07d9f6f19d8055c6e8c7a7b31581883
Travis Valenti <Bizarzoo10@ymail.com>
2015-04-16 15:46:22 +1000

    prepreprepreprepre-Alpha
    maybe one more pre?

---
+++ Pew-Pew/Assets/C#/developerConsole.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class developerConsole : MonoBehaviour {
+	public bool bDebug = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Backslash)) {
+			bDebug = !bDebug;
+		}
+	}
+	string command="";
+	void OnGUI() {
+		if (bDebug) {
+			command = GUI.TextField (new Rect (10, 10, 200, 20), command, 25);
+		}
+	}
+}
