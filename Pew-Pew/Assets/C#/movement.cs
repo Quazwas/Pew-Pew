82bb10992790ba56f54b62a85ad81f8c90508787
Kpaiy <craigjarrodonly@gmail.com>
2015-04-16 16:48:22 +1000

    Movement
    I think I did it.

---
+++ Pew-Pew/Assets/C#/movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	public GameObject player;
+	public Rigidbody rb;
+
+	public float speed = 5;
+
+
+	void move() {
+		rb.velocity = new Vector3 (Input.GetAxis("Horizontal") * speed, rb.velocity.y, Input.GetAxis("Vertical") * speed);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//network.isMine
+		if(network.isMine) {
+			move();
+		}
+	}
+}
